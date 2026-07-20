40b3aa60415b359e225dbdfa28125099db464a4d
thakurstsuman <156174516+thakurstsuman@users.noreply.github.com>
2026-07-20 13:52:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -32,5 +31,5 @@ def home():
     </html>
     """
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+# Vercel ke liye
+app = app
