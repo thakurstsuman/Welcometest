1a1bc68d16b7c50a3c1a641d7770072ae0446608
thakurstsuman <156174516+thakurstsuman@users.noreply.github.com>
2026-07-20 14:25:13 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,32 @@
-{
-  "version": 2,
-  "builds": [
-    {
-      "src": "api/index.py",
-      "use": "@vercel/python"
-    }
-  ],
-  "routes": [
-    {
-      "src": "/(.*)",
-      "dest": "api/index.py"
-    }
-  ]
-}
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Welcome</title>
+        <style>
+            body{
+                margin:0;
+                display:flex;
+                justify-content:center;
+                align-items:center;
+                height:100vh;
+                background:#111;
+                color:white;
+                font-family:Arial,sans-serif;
+            }
+            h1{
+                font-size:50px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Welcome To This Site</h1>
+    </body>
+    </html>
+    """
