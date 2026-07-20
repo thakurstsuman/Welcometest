87b13c4a5d8788fafda143b6bc57c428c91f2b25
thakurstsuman <156174516+thakurstsuman@users.noreply.github.com>
2026-07-20 10:26:58 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Welcome</title>
+        <style>
+            body{
+                margin:0;
+                display:flex;
+                justify-content:center;
+                align-items:center;
+                height:100vh;
+                background:#111;
+                color:white;
+                font-family:Arial,sans-serif;
+            }
+            h1{
+                font-size:50px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Welcome To This Site</h1>
+    </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
